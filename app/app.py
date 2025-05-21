0ab428d763ad2cbeb2d10343768c0ab5bd84432d
Ali Raheel <aliraheel626@gmail.com>
2025-05-21 15:12:39 +0500

    fixed issue with path prefix

--- app/app.py
+++ app/app.py
@@ -2,4 +2,4 @@ from fastapi import FastAPI
 from app.routers import websocket
 
 app = FastAPI()
-app.include_router(websocket.router, prefix="/ws/", tags=["messenger"])
+app.include_router(websocket.router, prefix="/ws", tags=["messenger"])
